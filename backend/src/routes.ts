9a44ac7711c0f3770d99237a946d9c9c76d47349
kengiiGMs <kaikiaoto22@gmail.com>
2023-12-06 20:44:40 -0300

    Desenvolvendo Middleware para Rotas Privadas + Buscando Detalhes do Usuário

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -2,11 +2,15 @@ import { Router } from "express";
 
 import { CreateUserController } from "./controllers/user/CreateUserController";
 import { AuthUserController } from "./controllers/user/AuthUserController";
+import { DetailUserController } from "./controllers/user/DetailUserController";
+
+import { isAuthenticated } from "./middlewares/isAuthenticated";
+
 const router = Router();
 
 /* Routes User */
 router.post('/users', new CreateUserController().handle);
-router.post('/session', new AuthUserController().handle)
-
+router.post('/session', new AuthUserController().handle);
+router.get('/me', isAuthenticated, new DetailUserController().handle);
 
 export { router }
---
+++ backend/src/services/user/DetailUserService.ts
@@ -0,0 +1,22 @@
+import { text } from "express";
+import prismaClient from "../../prisma";
+
+class DetailUserService {
+    async execute(user_id: string) {
+
+        const user = await prismaClient.user.findFirst({
+            where: {
+                id: user_id
+            },
+            select: {
+                id: true,
+                email: true,
+                name: true
+            }
+        })
+
+        return user
+    }
+}
+
+export { DetailUserService }
