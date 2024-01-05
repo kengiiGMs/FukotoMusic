a4a7dd2bb999f486bb43c83a906e5c844f9a5b25
kengiiGMs <kaikiaoto22@gmail.com>
2024-01-04 21:20:06 -0300

    Corrigindo a Verificação e Coleta de Informações do Usuário

--- frontend/src/contexts/AuthContext.tsx
+++ frontend/src/contexts/AuthContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, ReactNode, useState } from 'react'
+import { createContext, ReactNode, useState, useEffect, useDebugValue } from 'react'
 import { destroyCookie, setCookie, parseCookies } from 'nookies'
 
 import { api } from '../services/apiClient'
@@ -51,6 +51,27 @@ export function signOut() {
 export function AuthProvider({ children }: AuthProviderProps) {
     const [user, setUser] = useState<UserProps>()
     const isAuthenticated = !!user;
+
+    useEffect(() => {
+
+        const { '@nextauth.token': token } = parseCookies();
+
+        if (token) {
+            api.get('/me').then(response => {
+                const { id, name, email } = response.data;
+
+                setUser({
+                    id,
+                    name,
+                    email
+                })
+            })
+                .catch(() => {
+                    signOut();
+                })
+        }
+
+    }, [])
 
 
     async function signIn({ email, password }: SignInProps) {
